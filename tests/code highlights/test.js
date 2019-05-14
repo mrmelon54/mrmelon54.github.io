4fe8f73c95d346673288966e90286387ed9037ef
MrMelon54 <48031324+MrMelon54@users.noreply.github.com>
2019-05-14 09:57:11 +0100

    Update test.js

--- tests/code highlights/test.js
+++ tests/code highlights/test.js
@@ -1,1 +1,16 @@
+body {
+  background:#ddd;
+}
 
+#main {
+  background:white;
+  max-width:1000px;
+  width:100%;
+  margin-left:auto;
+  margin-right:auto;
+}
+
+p {
+  background:#0099ff;
+  color:white;
+}
